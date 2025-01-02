050a1560a6a01b3db7f2d165ff11f05e39e2927d
Eliseu costa <eliseualbano57@gmail.com>
2025-01-02 16:34:41 -0300

    criando o player parte 2

--- index.js
+++ index.js
@@ -6,26 +6,46 @@ canvas.height = innerHeight
 
 class Jogador {
     constructor() {
-        this.position = {
-            x: 200,
-            y: 200
-        }
+        
 
         this.velocidade = {
             x: 0,
             y: 0
         }
 
-        // this.image=
-        this.width = 100
-        this.height = 100
+        const imagem = new Image()
+        imagem.src = './images/spaceship.png'
+        imagem.onload = () => {
+            const escala = 0.15
+            this.image = imagem
+            this.width = imagem.width * escala
+            this.height = imagem.height * escala
+            this.position = {
+            x: canvas.width / 2 - this.width/ 2 ,
+            y: canvas.height - this.height- 20
+        }
+        }
+
     }
 
     desenhar() {
-        ctx.fillStyle = 'red'
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+        // ctx.fillStyle = 'red'
+        // ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+        if (this.image) {
+            ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+        }
+
     }
 }
 
-const jogador = new Jogador
-jogador.desenhar()
+const jogador = new Jogador()
+
+
+function animar() {
+    requestAnimationFrame(animar)
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    jogador.desenhar()
+}
+
+animar()
