ef2894ca36051b17341b0d03aa3a635f2c0762c3
Eliseu costa <eliseualbano57@gmail.com>
2025-01-02 14:52:55 -0300

    criando o player parte 1

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const canvas = document.querySelector('canvas')
-console.log(window.innerWidth)
-console.log(window.innerHeight)
 const ctx = canvas.getContext('2d')
 
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Jogador {
+    constructor() {
+        this.position = {
+            x: 200,
+            y: 200
+        }
+
+        this.velocidade = {
+            x: 0,
+            y: 0
+        }
+
+        // this.image=
+        this.width = 100
+        this.height = 100
+    }
+
+    desenhar() {
+        ctx.fillStyle = 'red'
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const jogador = new Jogador
+jogador.desenhar()
