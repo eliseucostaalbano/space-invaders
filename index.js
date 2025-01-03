c5cd9af52faf65575d9b15ba2cff555721e5ca93
Eliseu costa <eliseualbano57@gmail.com>
2025-01-03 14:12:47 -0300

    fazendo o player se mover parte 1

--- index.js
+++ index.js
@@ -31,21 +31,80 @@ class Jogador {
     desenhar() {
         // ctx.fillStyle = 'red'
         // ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
-        if (this.image) {
             ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
-        }
+        
 
     }
+
+  update(){
+    if(this.image){
+    this.desenhar()
+    this.position.x += this.velocidade.x
+    }
+    
+  }
 }
 
 const jogador = new Jogador()
-
+const setas = {
+    ArrowLeft: {
+        pressed : false
+    },
+    ArrowRight: {
+        pressed : false
+    },
+    space: {
+        pressed : false
+    }
+}
 
 function animar() {
     requestAnimationFrame(animar)
     ctx.fillStyle = 'black'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
-    jogador.desenhar()
+    jogador.update()
+
+    if (setas.ArrowLeft.pressed && jogador.position.x >= 0) {
+        jogador.velocidade.x = -5
+    } else if(setas.ArrowRight.pressed && jogador.position.x + jogador.width <= canvas.width) {
+        jogador.velocidade.x = 5
+    }else{
+        jogador.velocidade.x = 0
+    }
 }
 
 animar()
+
+addEventListener('keydown', ({key}) =>{
+    switch (key) {
+        case 'ArrowLeft':
+            console.log('esquerda')
+            setas.ArrowLeft.pressed = true
+            break;
+        case 'ArrowRight':
+            console.log('direita')
+            setas.ArrowRight.pressed = true
+            break;
+        case ' ':
+            console.log('espaço')
+            setas.space.pressed = true
+            break; 
+    }
+})
+
+addEventListener('keyup', ({key}) =>{
+    switch (key) {
+        case 'ArrowLeft':
+            console.log('esquerda')
+            setas.ArrowLeft.pressed = false
+            break;
+        case 'ArrowRight':
+            console.log('direita')
+            setas.ArrowRight.pressed = false
+            break;
+        case ' ':
+            console.log('espaço')
+            setas.space.pressed = true
+            break; 
+    }
+})
